c723b8a1da56a40776c6a98ebc03f80ea4d1a0c7
Simone Deponti <shywolf9982@gmail.com>
2010-06-06 23:23:19 +0000

    Completed refactor of: * Field tiles * Metadata tiles
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=37078

--- plone/app/standardtiles/field.py
+++ plone/app/standardtiles/field.py
@@ -1,6 +1,5 @@
-from zope.interface import alsoProvides
-from zope.component import getMultiAdapter
-from z3c.form.interfaces import IFieldWidget, IContextAware, DISPLAY_MODE
+from z3c.form.form import DisplayForm
+from z3c.form.field import Fields
 from plone.dexterity.utils import iterSchemata
 from plone.tiles import Tile
 from plone.supermodel.utils import mergedTaggedValueDict
@@ -8,34 +7,30 @@ from plone.autoform.interfaces import WIDGETS_KEY
 from plone.autoform.utils import resolveDottedName
 
 
-class DexterityFieldTile(Tile):
+class DexterityFieldTile(DisplayForm, Tile):
     """Field tile for Dexterity content
     """
+    
+    def __init__(self, context, request):
+        Tile.__init__(self, context, request)
+        DisplayForm.__init__(self, context, request)
+        self.fields = Fields()
+        for schema in iterSchemata(self.context):
+            self.fields += Fields(schema)
+
+    def updateWidgets(self):
+        for schema in iterSchemata(self.context):
+            widgets = mergedTaggedValueDict(schema, WIDGETS_KEY)
+            for name, factory in widgets.items():
+                if self.fields[name].widgetFactory.get(self.mode, None) is None:
+                    if isinstance(factory, basestring):
+                        factory = resolveDottedName(factory)
+                    self.fields[name].widgetFactory = factory
+        DisplayForm.updateWidgets(self)
 
     def _wrap_widget(self, render):
         return u"<html><body>%s</body></html>" % render
     
     def __call__(self):
-        for schema in iterSchemata(self.context):
-            if self.data['field'] in schema:
-                widgets = mergedTaggedValueDict(schema, WIDGETS_KEY)
-                if self.data['field'] in widgets:
-                    factory = widgets[self.data['field']]
-                    if isinstance(factory, basestring):
-                        factory = resolveDottedName(factory)
-                    widget = factory(schema[self.data['field']], self.request)
-                else:
-                    widget = getMultiAdapter(
-                        (schema[self.data['field']], self.request),
-                        IFieldWidget
-                    )
-                if not IContextAware.providedBy(widget):
-                    alsoProvides(widget, IContextAware)
-                widget.mode = DISPLAY_MODE
-                widget.context = self.context
-                widget.update()
-                return self._wrap_widget(widget.render())
-        raise KeyError("Context '%s' does not have a field named '%s'" % (
-            self.context,
-            self.data['field']
-        ))
+        self.update()
+        return self._wrap_widget(self.widgets[self.data['field']].render())
---
+++ plone/app/standardtiles/metadata.py
@@ -0,0 +1,50 @@
+from zope.interface import implements
+from plone.tiles.tile import Tile
+
+from plone.app.standardtiles.interfaces import IMetadataTile
+
+
+class BaseMetadataTile(Tile):
+    """The base class for metadata tiles (such as title and description)
+    """
+
+    implements(IMetadataTile)
+
+    def get_value(self):
+        return u""
+
+    @property
+    def value(self):
+        return self.get_value()
+
+    
+class DefaultTitleTile(BaseMetadataTile):
+    """A default tile for title
+    """
+
+    def get_value(self):
+        return u"Insert the content title here"
+
+    
+class DefaultDescriptionTile(BaseMetadataTile):
+    """A default tile for description
+    """
+
+    def get_value(self):
+        return u"Insert the content description here"
+
+    
+class DexterityTitleTile(BaseMetadataTile):
+    """A tile for dexterity content title
+    """
+
+    def get_value(self):
+        return self.context.title
+
+    
+class DexterityDescriptionTile(BaseMetadataTile):
+    """A tile for dexterity content description
+    """
+
+    def get_value(self):
+        return self.context.description
