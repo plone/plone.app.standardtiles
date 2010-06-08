79153fb75d7090cbf181b7c3cd7e320ff8ad6566
Rob Gietema <rob@fourdigits.nl>
2010-06-08 19:57:26 +0000

    Added site actions tile.
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=37114

--- plone/app/standardtiles/common.py
+++ plone/app/standardtiles/common.py
@@ -1,5 +1,6 @@
 from plone.tiles.tile import Tile
 from datetime import date
+from zope.component import getMultiAdapter
 
 
 class FooterTile(Tile):
@@ -9,3 +10,13 @@ class FooterTile(Tile):
     @property
     def year(self):
         return date.today().year
+
+
+class SiteActionsTile(Tile):
+    """A site actions tile
+    """
+
+    def site_actions(self):
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        return context_state.actions('site_actions')
