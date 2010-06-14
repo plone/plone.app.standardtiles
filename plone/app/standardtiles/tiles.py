bbd30ebab4fd82162eaee9ff0b7ba6d706843908
Rob Gietema <rob@fourdigits.nl>
2010-06-14 12:21:06 +0000

    Moved image / video to content.py.
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=37271

--- plone/app/standardtiles/tiles.py
+++ plone/app/standardtiles/tiles.py
@@ -56,21 +56,3 @@ class HelloNameTile(Tile):
     def __call__(self):
         return "<html><body>Hello %s!</body></html>" % self.data.get('name')
     
-
-class IYoutubeTile(directivesform.Schema):
-    
-    youtubeURL = schema.TextLine(title=u"Youtube URL", required=True)
-    
-
-
-class YoutubeTile(PersistentTile):
-    """A tile that displays a youtube movie. Purely as a proof of concept and to showcase possibilities of Deco
-    """
-    
-    def __call__(self):
-        youtubeURL = self.data.get('youtubeURL')
-        
-        youtubeID = re.split('v=([A-Za-z00-9_\-]+)', youtubeURL)[1]
-        
-        # Not for production use - this should be in a template!
-        return '<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/%s&hl=en_GB&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/%s&hl=en_GB&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed></object>' % (youtubeID, youtubeID)
