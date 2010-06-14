e0858fe5022c23de9bfe3769bdbba242f992f341
Rob Gietema <rob@fourdigits.nl>
2010-06-14 14:43:25 +0000

    Added personal bar tile.
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=37280

--- plone/app/standardtiles/common.py
+++ plone/app/standardtiles/common.py
@@ -3,6 +3,8 @@ from datetime import date
 from zope.component import getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
+from Acquisition import aq_inner
+from AccessControl import getSecurityManager
 
 
 class FooterTile(Tile):
@@ -43,3 +45,45 @@ class SkipLinksTile(Tile):
         context_state = getMultiAdapter((self.context, self.request),
                                         name=u'plone_context_state')
         return context_state.current_page_url
+
+
+class PersonalBarTile(Tile):
+    """A personal bar tile
+    """
+
+    def __init__(self, context, request):
+        Tile.__init__(self, context, request)
+
+        context = aq_inner(self.context)
+
+        context_state = getMultiAdapter((context, self.request),
+                                        name=u'plone_context_state')
+        self.portal_state = getMultiAdapter((context, self.request),
+                                            name=u'plone_portal_state')
+
+        sm = getSecurityManager()
+        self.user_actions = context_state.actions('user')
+        self.anonymous = self.portal_state.anonymous()
+
+        if not self.anonymous:
+            member = self.portal_state.member()
+            userid = member.getId()
+            
+            if sm.checkPermission('Portlets: View dashboard', context):
+                self.homelink_url = self.navigation_root_url + '/dashboard'
+            else:
+                self.homelink_url = self.navigation_root_url + \
+                    '/personalize_form'
+
+            membership = getToolByName(context, 'portal_membership')
+            member_info = membership.getMemberInfo(member.getId())
+            # member_info is None if there's no Plone user object, as when
+            # using OpenID.
+            if member_info:
+                fullname = member_info.get('fullname', '')
+            else:
+                fullname = None
+            if fullname:
+                self.user_name = fullname
+            else:
+                self.user_name = userid
