7e0212a4fdd731fc3d84e4bb953b76a446cfe19c
Rob Gietema <rob@fourdigits.nl>
2010-06-14 16:30:29 +0000

    Added content views and content actions tiles.
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=37288

--- plone/app/standardtiles/common.py
+++ plone/app/standardtiles/common.py
@@ -5,6 +5,8 @@ from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
 from Acquisition import aq_inner
 from AccessControl import getSecurityManager
+from plone.memoize.view import memoize
+from urllib import unquote
 
 
 class FooterTile(Tile):
@@ -210,3 +212,99 @@ class PathBarTile(Tile):
         breadcrumbs_view = getMultiAdapter((self.context, self.request),
                                            name='breadcrumbs_view')
         self.breadcrumbs = breadcrumbs_view.breadcrumbs()
+
+
+class ContentViewsTile(Tile):
+    """A content views tile
+    """
+
+    @memoize
+    def prepareObjectTabs(self, default_tab='view', sort_first=['folderContents']):
+        """Prepare the object tabs by determining their order and working
+        out which tab is selected. Used in global_contentviews.pt
+        """
+        context = aq_inner(self.context)
+        context_url = context.absolute_url()
+        context_fti = context.getTypeInfo()
+
+        context_state = getMultiAdapter(
+            (context, self.request), name=u'plone_context_state'
+        )
+        actions = context_state.actions
+
+        action_list = []
+        if context_state.is_structural_folder():
+            action_list = actions('folder')
+        action_list.extend(actions('object'))
+
+        tabs = []
+        found_selected = False
+        fallback_action = None
+
+        request_url = self.request['ACTUAL_URL']
+        request_url_path = request_url[len(context_url):]
+
+        if request_url_path.startswith('/'):
+            request_url_path = request_url_path[1:]
+
+        for action in action_list:
+            item = {'title'    : action['title'],
+                    'id'       : action['id'],
+                    'url'      : '',
+                    'selected' : False}
+
+            action_url = action['url'].strip()
+            starts = action_url.startswith
+            if starts('http') or starts('javascript'):
+                item['url'] = action_url
+            else:
+                item['url'] = '%s/%s' % (context_url, action_url)
+
+            action_method = item['url'].split('/')[-1]
+
+            # Action method may be a method alias:
+            # Attempt to resolve to a template.
+            action_method = context_fti.queryMethodID(
+                action_method, default=action_method
+            )
+            if action_method:
+                request_action = unquote(request_url_path)
+                request_action = context_fti.queryMethodID(
+                    request_action, default=request_action
+                )
+                if action_method == request_action:
+                    item['selected'] = True
+                    found_selected = True
+
+            current_id = item['id']
+            if current_id == default_tab:
+                fallback_action = item
+
+            tabs.append(item)
+
+        if not found_selected and fallback_action is not None:
+            fallback_action['selected'] = True
+
+        def sortOrder(tab):
+            try:
+                return sort_first.index(tab['id'])
+            except ValueError:
+                return 255
+
+        tabs.sort(key=sortOrder)
+        return tabs
+
+
+class ContentActionsTile(Tile):
+    """A content actions tile
+    """
+
+    def object_actions(self):
+        context = aq_inner(self.context)
+        context_state = getMultiAdapter((context, self.request),
+                                        name=u'plone_context_state')
+
+        return context_state.actions('object_actions')
+
+    def icon(self, action):
+        return action.get('icon', None)
