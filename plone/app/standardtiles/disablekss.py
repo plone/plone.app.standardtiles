7343d7bae86cfe744e7410f90c04446a9d9fa7a9
Israel Saeta Pérez <dukebody@gmail.com>
2010-07-22 23:36:09 +0000

    Disable kss inline validation temporarily.
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=38206

---
+++ plone/app/standardtiles/disablekss.py
@@ -0,0 +1,14 @@
+from kss.core import kssaction
+from plone.app.kss.plonekssview import PloneKSSView
+
+
+class NullFormValidation(PloneKSSView):
+    """Disable inline validation.
+
+    XXX: This horrible hack should only be a temporary solution until
+    we find a way to make plone.z3c.form calculate and use the form
+    name correctly.
+    """
+    @kssaction
+    def validate_input(self, *args):
+        return
