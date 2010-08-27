230b71acc385333c81150b0cc5eee8d08091ca33
Israel Saeta Pérez <dukebody@gmail.com>
2010-08-27 20:23:16 +0000

    Add configlets tile, with tests. Closes #10982.
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=39132

---
+++ plone/app/standardtiles/configlets.py
@@ -0,0 +1,10 @@
+from Products.CMFCore.utils import getToolByName
+
+from plone.tiles.tile import Tile
+
+class ConfigletsTile(Tile):
+    """A tile displaying the list of available configlets.
+    """
+
+    def controlPanel(self):
+        return getToolByName(self.context, 'portal_controlpanel')
