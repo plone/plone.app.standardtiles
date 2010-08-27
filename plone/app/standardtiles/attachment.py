56e4a753fde3e941349ded2bf6ed0be210246bf8
Jonas Baumann <j.baumann@4teamwork.ch>
2010-08-27 09:35:49 +0000

    attachment tile: added template with icon / filetype / size, changed schema to support multiple files but its not working very well yet
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=39052

--- plone/app/standardtiles/attachment.py
+++ plone/app/standardtiles/attachment.py
@@ -4,17 +4,19 @@ from zope.publisher.interfaces import NotFound
 from zope.publisher.interfaces import IPublishTraverse
 
 from plone.directives import form as directivesform
+from Products.CMFCore.utils import getToolByName
 
 from plone.tiles import PersistentTile
 
 from plone.namedfile.utils import set_headers, stream_data
 from plone.namedfile.field import NamedFile
-
+from plone.namedfile.interfaces import INamed
 
 class IAttachmentTile(directivesform.Schema):
 
-    fileObj = NamedFile(title=u"Please upload a file", required=True)
-    linkText = schema.TextLine(title=u"Link text", required=False)
+    files = schema.List(
+        title=u'Upload files',
+        value_type=NamedFile(title=u"Please upload a file", required=True))
 
 
 class AttachmentTile(PersistentTile):
@@ -35,17 +37,36 @@ class AttachmentTile(PersistentTile):
     """
 
     def __call__(self):
-        # Not for production use - this should be in a template!
-        fileObj = self.data.get('fileObj', None)
-        if fileObj is not None:
-            filename = fileObj.filename
-            linkText = self.data.get('linkText')
-            if linkText is None:
-                linkText = filename
-            fileURL = "%s/@@download/%s" % (self.url, filename,)
-            return '<html><body><a href="%s">%s</a></body></html>' % (fileURL, linkText)
+        self.update()
+        return self.index()
+
+    def update(self):
+        pass
+
+    def file_size(self, file_):
+        """Returns the file-size of the `file_` in KB.
+        """
+        if INamed.providedBy(file_):
+            return file_.getSize() / 1024
         else:
-            return '<html><body><em>File not found</em></body></html>'
+            return 0
+
+    def get_icon_for(self, file_):
+        """Returns the best icon for the `file_`
+        """
+        mtr = getToolByName(self.context, 'mimetypes_registry', None)
+        if mtr is None:
+            return self.context.getIcon()
+        lookup = mtr.lookup(file_.contentType)
+        if lookup:
+            mti = lookup[0]
+            try:
+                self.context.restrictedTraverse(mti.icon_path)
+                return mti.icon_path
+            except (NotFound, KeyError, AttributeError):
+                pass
+        return self.context.getIcon()
+
 
 class AttachmentTileDownload(object):
     """Implementation of the @@download view on the attachment tile.
@@ -66,13 +87,10 @@ class AttachmentTileDownload(object):
         """Render the file to the browser
         """
         
-        fileObj = self.context.data.get('fileObj', None)
-        if fileObj is None:
-            raise NotFound(self, self.filename, self.request)
-        
-        if not self.filename:
-            self.filename = getattr(fileObj, 'filename', '')
-        
-        set_headers(fileObj, self.request.response, filename=self.filename)
-        return stream_data(fileObj)
+        for file_ in self.context.data.get('files', ()):
+            if self.filename == getattr(file_, 'filename', ''):
+                set_headers(file_, self.request.response,
+                            filename=self.filename)
+                return stream_data(file_)
 
+        raise NotFound(self, self.filename, self.request)
