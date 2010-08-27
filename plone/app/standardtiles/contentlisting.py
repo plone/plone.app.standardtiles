ac2f5bb02253f70519800decaa15a9550fcffae5
Ralph Jacobs <ralph@fourdigits.nl>
2010-08-27 15:30:25 +0000

    implemented Contentlisting tile
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=39083

--- plone/app/standardtiles/contentlisting.py
+++ plone/app/standardtiles/contentlisting.py
@@ -1,17 +1,17 @@
 from zope import schema
-#from zope.app.component.hooks import getSite
 from zope.component import getMultiAdapter
 from plone.directives import form as directivesform
 from plone.tiles import PersistentTile
-#from plone.app.collection.browser.querybuilder import QueryBuilder
 from plone.formwidget.querystring.widget import QueryStringFieldWidget
+from plone.app.collection import queryparser
 
 
 class IContentListingTile(directivesform.Schema):
     """ A tile that displays the content in a folderish item """
     directivesform.widget(query=QueryStringFieldWidget)
     query = schema.List(title=u'Search terms',
-                        value_type=schema.Dict(value_type=schema.TextLine(), key_type=schema.TextLine()),
+                        value_type=schema.Dict(value_type=schema.TextLine(),
+                                               key_type=schema.TextLine()),
                         description=u'Define the search terms for the items '
                         'you want to list by choosing what to match on. The '
                         'list of results will be dynamically updated',
@@ -22,7 +22,7 @@ class IContentListingTile(directivesform.Schema):
 
 
 class ContentListingTile(PersistentTile):
-    """ A tile that displays the content in a folderish item """
+    """A tile that displays a listing of content items"""
 
     def __call__(self):
         self.update()
@@ -30,8 +30,14 @@ class ContentListingTile(PersistentTile):
 
     def update(self):
         self.query = self.data.get('query')
+        self.view_template = self.data.get('view_template')
 
     def SearchResults(self):
         """search results"""
+        parsedquery = queryparser.parseFormquery(self.context, self.query)
+        accessor = getMultiAdapter((self.context, self.request),
+            name='searchResults')(query=parsedquery)
+        view = 'collection_%s_view' % self.view_template
 
-        return getMultiAdapter((self.query, self.request), name='display_query_results')()
+        view = 'display_query_results'
+        return getMultiAdapter((accessor, self.request), name=view)()
