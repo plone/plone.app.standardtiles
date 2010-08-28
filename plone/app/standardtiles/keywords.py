c8235ce88ca557d89e60fb4b1a4ca3cfbddcdc4a
Israel Saeta Pérez <dukebody@gmail.com>
2010-08-28 17:39:34 +0000

    Cleanup code layout and drop extra whitespace.
    svn path=/sandbox/plone.app.standardtiles/trunk/; revision=39292

--- plone/app/standardtiles/keywords.py
+++ plone/app/standardtiles/keywords.py
@@ -4,30 +4,17 @@ from plone.app.standardtiles.utils import getNavigationRoot
 
 
 class KeywordsTile(PersistentTile):
-    
-    """ A tile that displays the context's keywords, if any """
-
-
-    def __call__(self):
-        self.update()
-        return self.index()
-
-
-    def update(self):
-        pass
-
+    """A tile that displays the context's keywords, if any.
+    """
 
     @property
     def categories(self):
-
-        """ Return context's categories """
+        """Return context's categories."""
 
         return self.context.Subject()
 
-
     def catUrl(self, category):
+        """Safely quote URL."""
 
-        """ Safely quote URL """
-      
         return "%s/search?Subject:list=%s" % \
-               (getNavigationRoot(self.context).absolute_url(), quote(category));
+               (getNavigationRoot(self.context).absolute_url(), quote(category))
