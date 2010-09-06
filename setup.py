290c4b3e48fe76f0bcf84a6a77b82d3859bbf198
Christian Schneider <daftdoggydog@googlemail.com>
2010-09-06 11:15:58 +0000

    Add plone.app.deco to dependencies
    svn path=/plone.app.standardtiles/trunk/; revision=39681

--- setup.py
+++ setup.py
@@ -3,30 +3,31 @@ import os
 
 version = '0.1'
 
-setup(name='plone.app.standardtiles',
-      version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
+setup(name = 'plone.app.standardtiles',
+      version = version,
+      description = "",
+      long_description = open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+      classifiers = [
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='Rob Gietema',
-      author_email='rob@fourdigits.nl',
-      url='http://plone.org',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+      keywords = '',
+      author = 'Rob Gietema',
+      author_email = 'rob@fourdigits.nl',
+      url = 'http://plone.org',
+      license = 'GPL',
+      packages = find_packages(exclude = ['ez_setup']),
+      namespace_packages = ['plone', 'plone.app'],
+      include_package_data = True,
+      zip_safe = False,
+      install_requires = [
           'setuptools',
           'lxml',
           'plone.app.dexterity',
+          'plone.app.deco',
           'plone.app.tiles',
           'plone.app.discussion',
           'plone.app.contentlisting',
@@ -39,14 +40,14 @@ setup(name='plone.app.standardtiles',
           'z3c.form',
           'five.grok',
       ],
-      extras_require={
+      extras_require = {
           'test': [
               'interlude',
               'plone.app.testing',
               'plone.app.dexterity',
               ]
           },
-      entry_points="""
+      entry_points = """
       [z3c.autoinclude.plugin]
       target = plone
       """,
