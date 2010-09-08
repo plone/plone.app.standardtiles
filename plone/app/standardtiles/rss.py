a9bec34d1343b5d8bf5e933b666c1e854471ce2a
Rob Gietema <rob@fourdigits.nl>
2010-09-08 18:52:08 +0000

    Added translatable strings to video tile.
    svn path=/plone.app.standardtiles/trunk/; revision=39793

--- plone/app/standardtiles/rss.py
+++ plone/app/standardtiles/rss.py
@@ -23,28 +23,28 @@ class IRSSTile(Schema):
     """
 
     portlet_title = schema.TextLine(
-        title=_(u'Title'),
-        description=_(u'Title of the portlet.  If omitted, the title of '
-                      'the feed will be used.'),
+        title=_(u"Title"),
+        description=_(u"Title of the portlet. If omitted, the title of the "
+                       "feed will be used."),
         required=False,
         default=u'')
 
     count = schema.Int(
-        title=_(u'Number of items to display'),
-        description=_(u'How many items to list.'),
+        title=_(u"Number of items to display"),
+        description=_(u"How many items to list."),
         required=True,
         default=5)
 
     url = schema.TextLine(
-        title=_(u'URL of RSS feed'),
-        description=_(u'Link of the RSS feed to display.'),
+        title=_(u"URL of RSS feed"),
+        description=_(u"Link of the RSS feed to display."),
         required=True,
         default=u'')
 
     timeout = schema.Int(
-        title=_(u'Feed reload timeout'),
-        description=_(u'Time in minutes after which the feed should be '
-                      'reloaded.'),
+        title=_(u"Feed reload timeout"),
+        description=_(u"Time in minutes after which the feed should be "
+                       "reloaded."),
         required=True,
         default=100)
 
