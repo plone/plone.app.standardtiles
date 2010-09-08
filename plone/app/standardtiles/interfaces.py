796171ecf33a98520f4896eeeb16367085a7ebb4
Rob Gietema <rob@fourdigits.nl>
2010-09-08 18:44:54 +0000

    Fixed translatable strings in navigation tile.
    svn path=/plone.app.standardtiles/trunk/; revision=39791

--- plone/app/standardtiles/interfaces.py
+++ plone/app/standardtiles/interfaces.py
@@ -15,10 +15,16 @@ class IMetadataTile(Interface):
 class IStandardTilesSettings(Interface):
     """Settings for standard tiles."""
     listing_views = schema.Dict(title=_(u"Listing views"),
-                                description=_(u"Listing views available for the content listing tile"),
+                                description=_(u"Listing views available for "
+                                               "the content listing tile"),
                                 key_type=schema.TextLine(),
                                 value_type=schema.TextLine())
 
     images_repo_path = schema.TextLine(title=_(u"Images repository path"),
-                                       description=_(u"The relative path to the folder containing the images available to select in the image tile, starting from the root of the site."),
+                                       description=_(u"The relative path to "
+                                                      "the folder containing "
+                                                      "the images available "
+                                                      "to select in the image "
+                                                      "tile, starting from "
+                                                      "the root of the site."),
                                        default=_(u"images"))
--- plone/app/standardtiles/navigation.py
+++ plone/app/standardtiles/navigation.py
@@ -28,71 +28,60 @@ from plone.app.layout.navigation.defaultpage import isDefaultPage
 from plone.app.layout.navigation.interfaces import INavtreeStrategy
 from plone.app.layout.navigation.interfaces import INavigationQueryBuilder
 
-
 from plone.app.standardtiles import PloneMessageFactory as _
+
 
 class INavigationTile(Schema):
     """A tile which can render the navigation tree
     """
 
     name = schema.TextLine(
-            title=_(u"label_navigation_title", default=u"Title"),
-            description=_(u"help_navigation_title",
-                          default=u"The title of the navigation tree."),
+            title=_(u"Title"),
+            description=_(u"The title of the navigation tree."),
             default=u"",
             required=False)
 
     root = schema.TextLine(
-            title=_(u"label_navigation_root_path", default=u"Root node"),
-            description=_(u'help_navigation_root',
-                          default=u"You may search for and choose a folder "
-                                   "to act as the root of the navigation "
-                                   "tree.  Leave blank to use the Plone "
-                                   "site root."),
+            title=_(u"Root node"),
+            description=_(u"You may search for and choose a folder to act as "
+                           "the root of the navigation tree.  Leave blank to "
+                           "use the Plone site root."),
             required=False)
 
     includeTop = schema.Bool(
-            title=_(u"label_include_top_node", default=u"Include top node"),
-            description=_(u"help_include_top_node",
-                          default=u"Whether or not to show the top, or "
-                                   "'root', node in the navigation tree. This "
-                                   "is affected by the 'Start level' "
-                                   "setting."),
+            title=_(u"Include top node"),
+            description=_(u"Whether or not to show the top, or 'root', node "
+                           "in the navigation tree. This is affected by the "
+                           "'Start level' setting."),
             default=False,
             required=False)
 
     currentFolderOnly = schema.Bool(
-            title=_(u"label_current_folder_only",
-                    default=u"Only show the contents of the current folder."),
-            description=_(u"help_current_folder_only",
-                          default=u"If selected, the navigation tree will "
-                                   "only show the current folder and its "
-                                   "children at all times."),
+            title=_(u"Only show the contents of the current folder."),
+            description=_(u"If selected, the navigation tree will only show "
+                           "the current folder and its children at all "
+                           "times."),
             default=False,
             required=False)
 
     topLevel = schema.Int(
-            title=_(u"label_navigation_startlevel", default=u"Start level"),
-            description=_(u"help_navigation_start_level",
-                default=u"An integer value that specifies the number of "
-                         "folder levels below the site root that must be "
-                         "exceeded before the navigation tree will display. 0 "
-                         "means that the navigation tree should be displayed "
-                         "everywhere including pages in the root of the site. "
-                         "1 means the tree only shows up inside folders "
-                         "located in the root and downwards, never showing "
-                         "at the top level."),
+            title=_(u"Start level"),
+            description=_(u"An integer value that specifies the number of "
+                           "folder levels below the site root that must be "
+                           "exceeded before the navigation tree will display. "
+                           "0 means that the navigation tree should be "
+                           "displayed everywhere including pages in the root "
+                           "of the site. 1 means the tree only shows up "
+                           "inside folders located in the root and downwards, "
+                           "never showing at the top level."),
             default=1,
             required=False)
 
     bottomLevel = schema.Int(
-            title=_(u"label_navigation_tree_depth",
-                    default=u"Navigation tree depth"),
-            description=_(u"help_navigation_tree_depth",
-                          default=u"How many folders should be included "
-                                   "before the navigation tree stops. 0 "
-                                   "means no limit. 1 only includes the "
-                                   "root folder."),
+            title=_(u"Navigation tree depth"),
+            description=_(u"How many folders should be included before the "
+                           "navigation tree stops. 0 means no limit. 1 only "
+                           "includes the root folder."),
             default=0,
             required=False)
 
