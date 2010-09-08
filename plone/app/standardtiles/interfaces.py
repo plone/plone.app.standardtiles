cf043c7c9994c4e3e670b55433860e6dad48460f
Rob Gietema <rob@fourdigits.nl>
2010-09-08 18:38:14 +0000

    Added translatable strings in registry settings.
    svn path=/plone.app.standardtiles/trunk/; revision=39788

--- plone/app/standardtiles/interfaces.py
+++ plone/app/standardtiles/interfaces.py
@@ -1,6 +1,8 @@
 from zope.interface import Interface
 from zope import schema
 
+from plone.app.standardtiles import PloneMessageFactory as _
+
 
 class IMetadataTile(Interface):
     """Metadata tiles are application tiles that handle metadata
@@ -12,11 +14,11 @@ class IMetadataTile(Interface):
 
 class IStandardTilesSettings(Interface):
     """Settings for standard tiles."""
-    listing_views = schema.Dict(title=u"Listing views",
-                                description=u"Listing views available for the content listing tile",
+    listing_views = schema.Dict(title=_(u"Listing views"),
+                                description=_(u"Listing views available for the content listing tile"),
                                 key_type=schema.TextLine(),
                                 value_type=schema.TextLine())
 
-    images_repo_path = schema.TextLine(title=u"Images repository path",
-                                      description=u"The relative path to the folder containing the images available to select in the image tile, starting from the root of the site.",
-                                      default=u"images")
+    images_repo_path = schema.TextLine(title=_(u"Images repository path"),
+                                       description=_(u"The relative path to the folder containing the images available to select in the image tile, starting from the root of the site."),
+                                       default=_(u"images"))
