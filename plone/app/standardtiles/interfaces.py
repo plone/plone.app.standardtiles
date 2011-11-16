d40e0ad62ea8936fba420c5234bf6a3bafc706cd
Ross Patterson <me@rpatterson.net>
2011-11-16 00:28:05 -0800

    Image tile has its own package

--- plone/app/standardtiles/interfaces.py
+++ plone/app/standardtiles/interfaces.py
@@ -1,7 +1,4 @@
 from zope.interface import Interface
-from zope import schema
-
-from plone.app.standardtiles import PloneMessageFactory as _
 
 
 class IMetadataTile(Interface):
@@ -11,15 +8,3 @@ class IMetadataTile(Interface):
     def get_value(self):
         """Returns the value to display through the template.
         """
-
-
-class IStandardTilesSettings(Interface):
-    """Settings for standard tiles."""
-    images_repo_path = schema.TextLine(title=_(u"Images repository path"),
-                                       description=_(u"The relative path to "
-                                                      "the folder containing "
-                                                      "the images available "
-                                                      "to select in the image "
-                                                      "tile, starting from "
-                                                      "the root of the site."),
-                                       default=_(u"images"))
