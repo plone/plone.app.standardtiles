c165fe6bb5826252424f561068f1a45f12cdd3f1
Ross Patterson <me@rpatterson.net>
2011-11-16 00:19:41 -0800

    The content listing tile has it's own package

--- plone/app/standardtiles/interfaces.py
+++ plone/app/standardtiles/interfaces.py
@@ -15,12 +15,6 @@ class IMetadataTile(Interface):
 
 class IStandardTilesSettings(Interface):
     """Settings for standard tiles."""
-    listing_views = schema.Dict(title=_(u"Listing views"),
-                                description=_(u"Listing views available for "
-                                               "the content listing tile"),
-                                key_type=schema.TextLine(),
-                                value_type=schema.TextLine())
-
     images_repo_path = schema.TextLine(title=_(u"Images repository path"),
                                        description=_(u"The relative path to "
                                                       "the folder containing "
