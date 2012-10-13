bea1a6110c24bc28b13ef278c4538e0894d98c72
Jean-François Roche <jfroche@affinitic.be>
2012-10-13 11:24:09 +0200

    PloneConf Sprint 2012: start again from scratch (with Rok G. benediction)

--- plone/app/standardtiles/utils.py
+++
@@ -1,9 +0,0 @@
-from Acquisition import aq_chain, aq_inner
-from plone.app.layout.navigation.interfaces import INavigationRoot
-
-
-def getNavigationRoot(context):
-    for obj in aq_chain(aq_inner(context)):
-        if INavigationRoot.providedBy(obj):
-            break
-    return obj
