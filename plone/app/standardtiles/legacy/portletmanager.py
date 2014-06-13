ae811ab90b92a3e677fb5ecb59846881a0d4ca03
Simone Orsi <simahawk@gmail.com>
2014-06-13 18:57:42 +0200

    fix redirection url for portlet manager renderered by tile

--- plone/app/standardtiles/legacy/portletmanager.py
+++ plone/app/standardtiles/legacy/portletmanager.py
@@ -28,13 +28,15 @@ class PortletManagerTile(Tile):
 
     def __call__(self):
         """Return the rendered contents of the portlet manager specified."""
+
         manager = self.data.get('manager')
         viewName = self.data.get('view')
-
         managerObj = getUtility(IPortletManager, name=manager)
-
         view = findView(self, viewName)
 
+        # set redirection view
+        self.request['viewname'] = '@@manage-portlets'
         rendererObj = managerObj(self.context, self.request, view)
         rendererObj.update()
+
         return u"<html><body>%s</body></html>" % rendererObj.render()
--- plone/app/standardtiles/legacy/utils.py
+++ plone/app/standardtiles/legacy/utils.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+
 from zope.browser.interfaces import IView
 from zope.component import queryMultiAdapter
 
