02ceb824af4774231a25685cb2d104b8fe4c275b
Asko Soukka <asko.soukka@iki.fi>
2015-05-05 03:48:05 +0300

    Refactor existing content tile to use rendering logic from collective.portlet.fullview; Remove plone.directives.form; Add Plone 4 support

--- plone/app/standardtiles/existingcontent.py
+++ plone/app/standardtiles/existingcontent.py
@@ -1,71 +1,56 @@
 # -*- coding: utf-8 -*-
-from lxml import etree
-from lxml import cssselect
-
-from plone.app.standardtiles import PloneMessageFactory as _ # noqa
-from plone.directives import form as directivesform
+from plone.app.uuid.utils import uuidToObject
+from plone.memoize.view import memoize
+from zope.browser.interfaces import IBrowserView
+from plone.app.standardtiles import PloneMessageFactory as _
+from plone.supermodel import model
 from plone.tiles import Tile
-from plone.app.vocabularies.catalog import CatalogSource
-from plone.app.z3cform.widget import RelatedItemsFieldWidget
-
+from plone.app.vocabularies.catalog import CatalogSource as CatalogSourceBase
 from zope import schema
 
 
-class IExistingContentTile(directivesform.Schema):
+class CatalogSource(CatalogSourceBase):
+    """ExistingContentTile specific catalog source to allow targeted widget
+    """
 
-    directivesform.widget(content_uid=RelatedItemsFieldWidget)
+
+class IExistingContentTile(model.Schema):
+
     content_uid = schema.Choice(
         title=_(u"Select an existing content"),
         required=True,
         source=CatalogSource(),
     )
-    css_selector = schema.TextLine(
-        title=_(u'CSS selector'),
-        description=_(u"Specify a CSS selector "
-                      u"to be used for getting "
-                      u"the HTML portion to inject."),
-        required=True,
-        default=u'#content'
-    )
-    view_name = schema.TextLine(
-        title=_(u"View name"),
-        required=False,
-        default=u'document_view',
-        # vocabulary='### what goes here??? should be dynamic'
-    )
 
 
 class ExistingContentTile(Tile):
-    """ Existing content tile.
+    """Existing content tile
     """
 
-    def get_content(self):
-        tools = self.context.restrictedTraverse('@@plone_tools')
-        catalog = tools.catalog()
-        content_uid = self.data.get('content_uid')
-        brain = catalog(UID=content_uid)
-        return brain and brain[0] or None
+    @property
+    @memoize
+    def content_context(self):
+        item = uuidToObject(self.data.get('content_uid'))
+        return item
 
-    def content_html(self):
-        brain = self.get_content()
-        if not brain:
-            return ''
+    @property
+    def default_view(self):
+        context = self.content_context
+        if context is not None:
+            item_layout = context.getLayout()
+            default_view = context.restrictedTraverse(item_layout)
+            return default_view
+        else:
+            return None
 
-        ps = self.context.restrictedTraverse('@@plone_portal_state')
-        portal = ps.portal()
-
-        view_name = self.data.get('view_name')
-        path = '{path}/{view}'.format(path=brain.getPath(),
-                                      view=view_name)
-
-        view = portal.restrictedTraverse(path)
-        css_selector = self.data.get('css_selector')
-
-        htmlparser = etree.HTMLParser()
-        tree = etree.fromstring(view(), htmlparser)
-        sel = cssselect.CSSSelector(css_selector)
-        content = sel(tree)
-        content_html = ''
-        if content:
-            content_html = etree.tostring(content[0])
-        return content_html
+    @property
+    def item_macros(self):
+        default_view = self.default_view
+        if default_view and IBrowserView.providedBy(default_view):
+            # IBrowserView
+            return default_view.index.macros
+        elif default_view:
+            # FSPageTemplate
+            return default_view.macros
+        else:
+            return None
