29ed8dedb747fabbf9a238b7d1c7f1daf2ab940a
Asko Soukka <asko.soukka@iki.fi>
2015-05-22 12:18:32 +0300

    Cleanup image tile implementation

--- plone/app/standardtiles/image.py
+++ plone/app/standardtiles/image.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-from Products.Five import BrowserView
 from plone.app.imaging.interfaces import IImagingSchema
 from plone.app.standardtiles import PloneMessageFactory as _
 from plone.namedfile.field import NamedBlobFile
-from plone.namedfile.utils import set_headers
-from plone.namedfile.utils import stream_data
 from plone.supermodel.model import Schema
 from plone.tiles import PersistentTile
 from zope import schema
 from zope.component.hooks import getSite
 from zope.interface import alsoProvides
-from zope.interface import implementer
 from zope.interface import provider
-from zope.publisher.interfaces import IPublishTraverse
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
@@ -50,12 +45,13 @@ class IImageTile(Schema):
 class ImageTile(PersistentTile):
     """Image tile.
 
-    This is a persistent tile which stores a image and
+    This is a persistent tile which stores an image with
     selected image scale.
+
     When rendered, the tile will output an <img /> tag like::
 
-    <img src="http://.../@@plone.app.standardtiles.attachment/tile-id/
-    @@images/filename.png" />
+    <img src=".../@@plone.app.standardtiles.image/tile-id/
+              @@images/bcc7be5d-75cc-4cfd-9d3a-4ad231aa01de.png" />
 
     """
 
@@ -67,60 +63,3 @@ class ImageTile(PersistentTile):
         # Disable CSRF, because imagescales may save a new scale into ZODB
         if HAS_PLONE_PROTECT:
             alsoProvides(self.request, IDisableCSRFProtection)
-
-
-
-@implementer(IPublishTraverse)
-class ImageDownload(BrowserView):
-    def __init__(self, context, request):
-        super(ImageDownload, self).__init__(context, request)
-        self.stack = []
-
-    def publishTraverse(self, request, name):
-        self.stack.append(name)
-        return self
-
-    def render(self):
-        download = self.context.image.restrictedTraverse("@@download")
-        for part in self.stack:
-            download = download.publishTraverse(self.request, part)
-        # Fix filename with non-ascii characters to be encoded as utf-8
-        if download.filename is None:
-            file_ = download._getFile()
-            filename = getattr(file_, 'filename', download.fieldname)
-            if type(filename) == unicode:
-                download.filename = filename.encode('utf-8', 'ignore')
-        return download()
-
-
-@implementer(IPublishTraverse)
-class ImageScales(BrowserView):
-    def __init__(self, context, request):
-        super(ImageScales, self).__init__(context, request)
-        self.stack = []
-
-    def publishTraverse(self, request, name):
-        self.stack.append(name)
-        return self
-
-    def render(self):
-        """The correct URL to scaled images we want to publish regardless
-        of publication state is of the form:
-            tile-url/@@images/scaled-imagefile-name
-        """
-        images = self.context.image.restrictedTraverse("@@images")
-        scale = None
-
-        # Traverse the url one part at a time, until we reach the real image
-        for part in self.stack:
-            scale = images.publishTraverse(self.request, part)
-            if scale != images:
-                # we're out of the original context (and probably in the
-                # imagescale object, which can't be traversed)
-                break
-
-        if scale:
-            set_headers(scale.data, self.request.response)
-            return stream_data(scale.data)
-        else:
-            return images
