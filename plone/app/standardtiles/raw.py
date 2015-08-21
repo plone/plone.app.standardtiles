d9df55b72cc55a1362596c89d68b1c0b3a72983f
vangheem <vangheem@gmail.com>
2015-08-21 00:06:11 -0500

    should just be persistent always

--- plone/app/standardtiles/raw.py
+++ plone/app/standardtiles/raw.py
@@ -7,7 +7,7 @@
 
 from plone.app.standardtiles import _PMF as _
 from plone.supermodel.model import Schema
-from plone.tiles import Tile
+from plone.tiles import PersistentTile
 from zope import schema
 
 
@@ -19,6 +19,6 @@ class IRawHTMLTile(Schema):
     )
 
 
-class RawHTMLTile(Tile):
+class RawHTMLTile(PersistentTile):
     def __call__(self):
         return u"<html><body>%s</body></html>" % (self.data.get('content') or '<p></p>')
