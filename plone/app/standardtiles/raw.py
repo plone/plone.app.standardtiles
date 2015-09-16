3beedf2f3a7ba2105f5c966ba72fc6b75dd45e72
Asko Soukka <asko.soukka@iki.fi>
2015-09-16 16:17:01 +0300

    Fix to apply output filters for rawhtml tile

--- plone/app/standardtiles/raw.py
+++ plone/app/standardtiles/raw.py
@@ -4,11 +4,13 @@
 # because it is assumed this will only be used in layouts
 # and not added to custom layouts since custom layouts will
 # use normal text input
-
 from plone.app.standardtiles import _PMF as _
+from plone.outputfilters import apply_filters
+from plone.outputfilters.interfaces import IFilter
 from plone.supermodel.model import Schema
 from plone.tiles import PersistentTile
 from zope import schema
+from zope.component import getAdapters
 
 
 class IRawHTMLTile(Schema):
@@ -21,4 +23,13 @@ class IRawHTMLTile(Schema):
 
 class RawHTMLTile(PersistentTile):
     def __call__(self):
-        return u"<html><body>%s</body></html>" % (self.data.get('content') or '<p></p>')
+        content = self.data.get('content')
+        if content:
+            # Here we skip legacy portal_transforms and call
+            # plone.outputfilters directly by purpose
+            filters = [f for _, f
+                       in getAdapters((self.context, self.request), IFilter)]
+            content = apply_filters(filters, content)
+        else:
+            content = u'<p></p>'
+        return u"<html><body>%s</body></html>" % content
