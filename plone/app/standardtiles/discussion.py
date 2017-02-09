c7aeb3c90a0301bf0e41eb8060d492008b36b2e8
Asko Soukka <asko.soukka@iki.fi>
2017-02-09 14:48:41 +0200

    Fix discussion tile to properly post to tile URL and redirect to context URL; Add explicit CSRF-protection to support rendering as ESI tile

--- plone/app/standardtiles/discussion.py
+++ plone/app/standardtiles/discussion.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from lxml import etree
+from lxml import html
 from plone.app.discussion.browser.comments import CommentForm
 from plone.app.layout.globals.interfaces import IViewView
+from plone.protect import createToken
 from plone.tiles import Tile
+from repoze.xmliter.utils import getHTMLSerializer
 from zope.component import queryMultiAdapter
 from zope.interface import alsoProvides
 from zope.viewlet.interfaces import IViewlet
@@ -10,6 +14,27 @@ from zope.viewlet.interfaces import IViewletManager
 
 class TileCommentForm(CommentForm):
     prefix = 'plone.app.standardtiles.discussion'
+    action = ''
+
+
+def protect(raw):
+    parser = getHTMLSerializer(
+        raw.encode('utf-8'), pretty_print=False, encoding='utf-8')
+    parser.serializer = html.tostring
+
+    root = parser.tree.getroot()
+    token = createToken()
+
+    for form in root.cssselect('form'):
+        authenticator = form.cssselect('[name="_authenticator"]')
+        if len(authenticator) == 0:
+            authenticator = etree.Element("input")
+            authenticator.attrib['name'] = '_authenticator'
+            authenticator.attrib['type'] = 'hidden'
+            authenticator.attrib['value'] = token
+            form.append(authenticator)
+
+    return (''.join(parser)).decode('utf-8')
 
 
 class DiscussionTile(Tile):
@@ -29,12 +54,15 @@ class DiscussionTile(Tile):
             viewlet.form = TileCommentForm
             viewlet.update()
 
+            if isinstance(viewlet.form, TileCommentForm):
+                viewlet.form.action = self.url
+
             # Fix submit redirect from tile to context
             if 'location' in self.request.response.headers:
-                self.request.response.redirect(
-                    self.request.response.headers['location'].replace(
-                        self.url, self.context.absolute_url()))
-
-            return u'<html><body>%s</body></html>' % viewlet.render()
+                location = self.request.response.getHeader('location')
+                self.request.response.redirect(''.join([
+                    self.context.absolute_url(), '#', location.split('#')[-1]
+                ]))
+            return protect(u'<html><body>%s</body></html>' % viewlet.render())
         else:
-            return u'<html></html>'
+            return u'<html><body></body></html>'
