c6fc1e0b18131c1381d5844fdfacf7a2e63890b3
Jens W. Klein <jk@kleinundpartner.at>
2018-04-13 11:34:05 +0200

    fix test setup - six and requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@
 rm -r ./lib ./include ./local ./bin
 virtualenv --clear .
 ./bin/pip install -U pip
-./bin/pip install -r requirements.txt
+./bin/pip install -r https://github.com/plone/buildout.coredev/raw/5.1/requirements.txt
 ./bin/buildout $*
