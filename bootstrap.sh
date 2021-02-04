e56440f13ff87a9c00d45e8ab52e03715678afbb
Dylan Jay <software@pretaweb.com>
2021-02-04 11:33:35 +0700

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
-#rm -rf ./lib ./include ./local ./bin
-virtualenv --clear .
-ls -al ./bin
-bin/pip install --upgrade pip setuptools zc.buildout
-bin/buildout $* annotate | tee annotate.txt | grep -E 'setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
+# rm -rf ./lib ./include ./local ./bin
+# virtualenv --clear .
+# ls -al ./bin
+[ ! -f bin/pip ] && virtualenv .
+# Determine setuptools and buildout versions from annotate using latest buildout
+./bin/pip install --upgrade pip setuptools zc.buildout
+./bin/buildout $* annotate | tee annotate.txt | grep -E 'setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
 cat annotate.txt
 cat requirements.txt
-bin/pip install --upgrade -r requirements.txt
-bin/buildout $*
+# Reset out virtualenv to the versions we need so buildout doesn't have to upgrade
+./bin/pip install --upgrade -r requirements.txt
+# run buildout
+./bin/buildout $*
