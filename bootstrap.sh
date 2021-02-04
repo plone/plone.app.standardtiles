d2242617ec42c204e40e92694e1aeaaf68923a7c
Dylan Jay <software@pretaweb.com>
2021-02-04 11:06:32 +0700

    fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 rm -r ./lib ./include ./local ./bin
-[ ! -f bin/pip ] && virtualenv --clear .
+virtualenv --clear .
 bin/pip install --upgrade pip setuptools zc.buildout
 bin/buildout $* annotate | tee annotate.txt | grep -E 'setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
 cat annotate.txt
