4ba790c51944ec7280c28143a890dfac60125715
ksuess <k.suess@rohberg.ch>
2021-12-16 16:48:05 +0100

    Update bootstrap.sh: Determine pip version from annotate using latest buildout

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 [ ! -f bin/pip ] && virtualenv .
 # Determine setuptools and buildout versions from annotate using latest buildout
 ./bin/pip install --upgrade pip setuptools zc.buildout
-./bin/buildout $* annotate | tee annotate.txt | grep -E 'setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
+./bin/buildout $* annotate | tee annotate.txt | grep -E 'pip *= *[0-9][^ ]*|setuptools *= *[0-9][^ ]*|zc.buildout *= *[0-9][^ ]*'| sed 's/= /==/' > requirements.txt
 cat annotate.txt
 cat requirements.txt
 # Reset out virtualenv to the versions we need so buildout doesn't have to upgrade
